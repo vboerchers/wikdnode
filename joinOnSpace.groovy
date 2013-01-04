6d1d4065135aced61ab15cfe761d68e0eff2a357
lcr <none@none>
2013-01-03 18:39:37 -0700

    - tweak

--- joinOnSpace.groovy
+++ joinOnSpace.groovy
@@ -1,3 +1,4 @@
 def result = []
-c.selecteds.every { i -> result.push(i.plainText) }
-node.createChild(result.join('. '))
+c.selecteds.every { i -> result.push(i.plainText.trim()) }
+node.text = result.join(' ')
+c.selecteds.tail().each { it.delete() }
