9f30d3755b102be5b2d2ec767d21301fb1da2442
lcr <none@none>
2013-01-05 00:56:20 -0700

    - style sync script

---
+++ syncStyleWithIcons.groovy
@@ -0,0 +1,13 @@
+def thisStyle = node?.style?.style
+def thisIcons = node?.icons?.icons
+def styleIcons = node?.style?.styleNode?.icons?.icons
+c.selecteds.findAll { it?.id != node?.id }.each {
+   if (thisStyle)
+      it?.style?.style = thisStyle
+    // sync any local icons
+    if (thisIcons)
+      thisIcons?.every { icon -> it?.icons?.add(icon) }
+    // sync any style icons
+     if (styleIcons)
+       styleIcons?.every { icon -> it?.icons?.add(icon) }
+}
