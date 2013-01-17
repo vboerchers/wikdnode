4b25635a1ea4d74bf27d26400f51d04a5b748545
lcr <none@none>
2013-01-17 15:04:38 -0700

    - "wikdNode" delete* handles "Cancel" properly

--- scripts/deleteFirstChars.groovy
+++ scripts/deleteFirstChars.groovy
@@ -4,9 +4,10 @@ import java.util.regex.Pattern
 import org.apache.commons.lang.StringUtils as SU
 
 def trimPrompt(String pattern, Boolean start, String msg) {
-	def mark = Pattern.quote(ui.showInputDialog(node.delegate, msg, ''))
-	mark = mark ? mark : ''
-	mark = start ? pattern + mark : mark + pattern
+	def mark = ui.showInputDialog(node.delegate, msg, '')
+	if (mark != null) {
+	   mark = Pattern.quote(start ? pattern + mark : mark + pattern)
+	}
 	c.selecteds.every { it.text = SU.trim( it.plainText.replaceFirst(mark, '')) }
 }
 trimPrompt('^{1}', true, 'Enter characters to delete from text start...')
